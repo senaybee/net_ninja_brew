4b90a88c301d97bf7810eae4745554f232ffd45f
senaybee <senay_berihu@outlook.com>
2020-07-20 15:43:46 -0700

    Create stream to see if user is logged in or not

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,24 +11,26 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.brown[100],
-        appBar: AppBar(
-            backgroundColor: Colors.brown[400],
-            elevation: 0.0,
-            title: Text('Sign in to Brew Crew')),
-        body: Container(
-            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
-            child: RaisedButton(
-              child: Text('Sign in anon'),
-              onPressed: () async {
-                dynamic result = await _authService.signInAnon();
-                if (result == null) {
-                  print('Error signing in');
-                } else {
-                  print('User has signed in');
-                  print(result.uid);
-                }
-              },
-            )));
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          title: Text('Sign in to Brew Crew')),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
+        child: RaisedButton(
+          child: Text('Sign in anon'),
+          onPressed: () async {
+            dynamic result = await _authService.signInAnon();
+            if (result == null) {
+              print('Error signing in');
+            } else {
+              print('User has signed in');
+              print(result.uid);
+            }
+          },
+        ),
+      ),
+    );
   }
 }
