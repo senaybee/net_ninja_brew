2d8dbf15e34abf6f76bf3e9f36b8eba9bea3be21
senaybee <senay_berihu@outlook.com>
2020-07-21 14:51:52 -0700

    Implement toggle functionality between sign in and register forms

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:net_ninja_brew/services/auth.dart';
 
-
 class Register extends StatefulWidget {
+  final Function onToggle;
+  Register({this.onToggle});
+
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-  
   final AuthService _authService = AuthService();
   String email = '';
   String password = '';
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          title: Text('Sign in to Brew Crew')),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to Brew Crew'),
+        actions: [
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Sign in'),
+            onPressed: () {
+              widget.onToggle();
+            },
+          )
+        ],
+      ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
         child: Form(
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:net_ninja_brew/services/auth.dart';
 
 class SignIn extends StatefulWidget {
+  final Function onToggle;
+
+  SignIn({this.onToggle});
+
   @override
   _SignInState createState() => _SignInState();
 }
@@ -16,9 +20,19 @@ class _SignInState extends State<SignIn> {
     return Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          title: Text('Sign in to Brew Crew')),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to Brew Crew'),
+        actions: [
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Register'),
+            onPressed: () {
+              widget.onToggle();
+            },
+          ),
+        ],
+      ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
         child: Form(
