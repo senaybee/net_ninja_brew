10fab12ea3e8ede3be4f326232cb2e1c48d5a53b
senaybee <senay_berihu@outlook.com>
2020-07-21 15:34:38 -0700

    Implement singin functionality

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -14,6 +14,8 @@ class _SignInState extends State<SignIn> {
   final AuthService _authService = AuthService();
   String email = '';
   String password = '';
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +38,13 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (value) =>
+                    value.isEmpty ? 'Please enter an email' : null,
                 onChanged: (value) {
                   setState(() {
                     email = value;
@@ -48,6 +53,9 @@ class _SignInState extends State<SignIn> {
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                 validator: (value) => value.length < 6
+                    ? 'Please enter a password longer than 6 characters'
+                    : null,
                 onChanged: (value) {
                   setState(() {
                     password = value;
@@ -58,7 +66,15 @@ class _SignInState extends State<SignIn> {
               SizedBox(height: 20.0),
               RaisedButton(
                 onPressed: () async {
-                  print('$email \n $password');
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _authService
+                        .signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() {
+                        error = 'Something went wrong';
+                      });
+                    }
+                  }
                 },
                 color: Colors.pink[400],
                 child: Text(
@@ -66,6 +82,11 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
               ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0),
+              )
             ],
           ),
         ),
