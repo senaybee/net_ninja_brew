9337499a6fbb6f33af62416fca7e06ba80ec44d9
senaybee <senay_berihu@outlook.com>
2020-07-22 15:19:49 -0700

    Set up Firestore

---
+++ lib/services/database.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  //Collection reference
+  final CollectionReference brewCollection =
+      Firestore.instance.collection('brews');
+}
