bd3338144358f66b631edcf8db3c4b459da64e55
senaybee <senay_berihu@outlook.com>
2020-07-23 14:50:08 -0700

    Fetch and display dummy data from firestore

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:net_ninja_brew/models/brew.dart';
+import 'package:net_ninja_brew/screens/home/brew_tile.dart';
+import 'package:net_ninja_brew/shared/loading.dart';
 import 'package:provider/provider.dart';
 
 class BrewList extends StatefulWidget {
@@ -12,11 +14,11 @@ class _BrewListState extends State<BrewList> {
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
     //print(brews.documents);
-    brews.forEach((element) {
-      print(element.name);
-      print(element.sugars);
-      print(element.strength);
-    });
-    return Container();
+    return brews == null? Loading() : ListView.builder(
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+      itemCount: brews.length,
+    );
   }
 }
