b2c0091894a8dc78c00187c3fee28226338491ae
senaybee <senay_berihu@outlook.com>
2020-07-24 14:41:42 -0700

    Define UserData model and stream

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:net_ninja_brew/models/brew.dart';
+import 'package:net_ninja_brew/models/user.dart';
 
 class DatabaseService {
   final String uid;
@@ -29,4 +30,19 @@ class DatabaseService {
   Stream<List<Brew>> get brews {
     return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
+
+  //userData from snapshot
+  UserData _userDataFromSnapshot(DocumentSnapshot snapshot) {
+    return UserData(
+        uid: uid,
+        sugars: snapshot.data['sugars'],
+        name: snapshot.data['name'],
+        strength: snapshot.data['strength']);
+  }
+
+  //get user doc stream
+  Stream<UserData> get userData {
+    return brewCollection.document(uid).snapshots().map(_userDataFromSnapshot);
+  }
 }
+   
